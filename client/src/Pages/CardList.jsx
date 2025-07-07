13820f625737f5957ca532f219664f5d32ff11ab
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2025-07-07 22:03:35 +0200

    fix responsive layout in cardList

--- client/src/Pages/CardList.jsx
+++ client/src/Pages/CardList.jsx
@@ -5,7 +5,7 @@ const CardList = ({questions}) => {
   return (
     <div
       className={
-        "grid grid-cols-2 sm:grid-cols-3 place-items-center gap-4 mt-20 min-h-[calc(100vh-100px)] p-4"
+        "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-4 mt-20 min-h-[calc(100vh-100px)] p-4"
       }>
       {questions &&
         questions.map((q) => (
