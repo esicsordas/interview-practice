7ce2383ccb59eea46da7e0c295b970a7deba4f06
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2025-07-10 15:38:48 +0200

    add Hashrouter instead of Browserrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ApiProvider } from "./AppContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ApiProvider>
       <App />
     </ApiProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
